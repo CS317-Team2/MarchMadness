bd6bbc56348f2718d1e1801b66bfc4b1681b4b8f
PXIONG001 <61022244+PXIONG001@users.noreply.github.com>
2020-10-13 12:35:46 -0500

    Add files via upload
    Peng: Created a file tester. It is not finished yet. I will finish it at some point tonight.

---
+++ src/FileTester.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ * @author pengs
+ *
+ */
+public class FileTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws FileNotFoundException{
+		File file = new File("mcb2019CSV.csv");
+		Scanner sc = new Scanner(file);
+		
+		//TODO finish the file tester
+	}
+
+}
