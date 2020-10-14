94677f824cc90a6c23a0bd9d70ea3ca4d5b3202e
Peng Xiong <PXIONG001@csbsju.edu>
2020-10-14 02:21:24 -0500

    Peng: Attempted to test the file tester, but failed. I will look at it tomorrow.

--- src/FileTester.java
+++ src/FileTester.java
@@ -1,6 +1,7 @@
 // Import File, FileNotFOundException
 import java.io.*;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 // Import Scanner
 import java.util.Scanner;
 
@@ -26,24 +27,52 @@ public class FileTester {
 		
 		Scanner sc = new Scanner(file);
 		
+		ArrayList<String> date_of_game = new ArrayList<String>();
 		ArrayList<String> team_1 = new ArrayList<String>();
 		ArrayList<String> team_2 = new ArrayList<String>();
-		ArrayList<Double> team_1_score = new ArrayList<Double>();
-		ArrayList<Double> team_2_score = new ArrayList<Double>();
+		ArrayList<Integer> team_1_score = new ArrayList<Integer>();
+		ArrayList<Integer> team_2_score = new ArrayList<Integer>();
 		
 		sc.useDelimiter(",");
 		
-		while(sc.hasNext()) {
-			team_1.add(sc.nextLine());
-			team_2.add(sc.nextLine());
-			//team_1_score.add(sc.nextDouble());
-			//team_2_score.add(sc.nextDouble());
+		try {
+			while(sc.hasNext()) {
+				//System.out.println(sc.next());
+				
+				// Date of the game
+				String date = sc.next();
+				date_of_game.add(date);
+				System.out.println(date_of_game);
+				
+				// First team name
+				String first_team_name = sc.next();
+				team_1.add(first_team_name);
+				System.out.println(team_1);
+				
+				// First team score
+				int first_team_score = sc.nextInt();
+				team_1_score.add(first_team_score);
+				System.out.println(team_1_score);
+				
+				// Second team name
+				String second_team_name = sc.next();
+				team_2.add(second_team_name);
+				System.out.println(team_2);
+				
+				// Second team score
+				int second_team_score = sc.nextInt();
+				team_2_score.add(second_team_score);
+				System.out.println(team_2_score);
+				
+			}
 		}
 		
-		System.out.println(team_1);
-		System.out.println(team_2);
+		catch(InputMismatchException ex) {
+			System.out.println("Not an integer!");
+		}
+		
 		// Closes the scanner
-		sc.close();
+		sc.close();		
 		
 	}
 
