71ae40bf46a71ff5fd0a5146b883af56fde68b12
Peng Xiong <PXIONG001@csbsju.edu>
2020-10-13 23:36:52 -0500

    Peng: Attempted test for file tester. Still needs to finish

--- src/FileTester.java
+++ src/FileTester.java
@@ -1,6 +1,6 @@
 // Import File, FileNotFOundException
 import java.io.*;
-
+import java.util.ArrayList;
 // Import Scanner
 import java.util.Scanner;
 
@@ -20,10 +20,31 @@ public class FileTester {
 	 * @param args
 	 */
 	public static void main(String[] args) throws FileNotFoundException{
+		
+		// File name
 		File file = new File("C:\\Users\\pengs\\eclipse-workspace\\MarchMadness\\data\\mcb2019CSV.csv");
+		
 		Scanner sc = new Scanner(file);
+		
+		ArrayList<String> team_1 = new ArrayList<String>();
+		ArrayList<String> team_2 = new ArrayList<String>();
+		ArrayList<Double> team_1_score = new ArrayList<Double>();
+		ArrayList<Double> team_2_score = new ArrayList<Double>();
+		
+		sc.useDelimiter(",");
+		
+		while(sc.hasNext()) {
+			team_1.add(sc.nextLine());
+			team_2.add(sc.nextLine());
+			//team_1_score.add(sc.nextDouble());
+			//team_2_score.add(sc.nextDouble());
+		}
+		
+		System.out.println(team_1);
+		System.out.println(team_2);
+		// Closes the scanner
 		sc.close();
-		//TODO finish the file tester
+		
 	}
 
 }
